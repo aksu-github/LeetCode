c7e636d70243b2402f017438377ad25143305aaf
Alvin Su <su.alvin@proton.me>
2022-11-15 19:12:47 -0800

    11/15 - Update 3

---
+++ Easy/js/11 15 - Easy - Sqrt(x).js
@@ -0,0 +1,23 @@
+/*
+Instruction:
+
+Given a non-negative integer x, 
+return the square root of x rounded down to the nearest integer. 
+
+The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+*/
+
+//  My Solution 
+
+/*
+ * @param {number} x
+ * @return {number}
+ */
+ var mySqrt = function(x) {
+  return Math.floor(Math.sqrt(x));
+};
