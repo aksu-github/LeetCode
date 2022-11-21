7ca891fbca1d437436abfcc788beeac2e90044e8
Alvin Su <su.alvin@proton.me>
2022-11-20 19:25:37 -0800

    11/20 - Update 2

--- Easy/js/11 19 - Easy - Binary Search Inorder Traversal.js
+++ Easy/js/11 19 - Easy - Binary Search Inorder Traversal.js
@@ -7,26 +7,7 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
 
 //  My Solution 
 
-/*
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
- var merge = function(nums1, m, nums2, n) {
-    let total = m + n - 1;
 
-    for ( let i = nums1.length - 1; i > 0, n > 0; i--, total--){
-        if ( nums1[m-1] > nums2[n-1] ){
-            nums1[i] = nums1[m-1];
-            m--;
-        } else {
-            nums1[i] = nums2[n-1];
-            n--;
-        }
-    }
-};
 
 //  LeetCode - Recursive (Simple, but resource and time intensive)
 
---
+++ Easy/js/11 20 - Easy - Same Tree.js
@@ -0,0 +1,59 @@
+/*
+Instruction:
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+*/
+
+//  My Solution 
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+  if ( !p && !q ){
+      return 1;
+  }
+
+  if ( !p && q ){
+      return 0;
+  } else if ( p && !q ){
+      return 0;
+  } else {
+      if ( p.val == q.val && isSameTree(p.left, q.left) == 1 && isSameTree(p.right, q.right)== 1 ){
+          return 1;
+      } else {
+          return 0;
+      }
+  }
+};
+
+
+//  LeetCode - Fastest Runtime && Least Memory Usage
+
+var isSameTree = function(p, q) {
+  //if they both don't exist return true
+  if (!p && !q) return true
+
+  //if there is either p or q but not the other, return false
+  if (!p || !q) return false
+
+  //if they two current nodes are unequal, return false
+  return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+};
+
+
+
