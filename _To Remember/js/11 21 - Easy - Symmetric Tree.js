e1ab70a62e4e37de7744a8573f9c871d75ccad20
Alvin Su <su.alvin@proton.me>
2022-11-21 23:36:18 -0800

    11/21 - Update 2

---
+++ _To Remember/js/11 21 - Easy - Symmetric Tree.js
@@ -0,0 +1,61 @@
+/*
+Instruction:
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+*/
+
+//  My Solution 
+
+/*
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/*
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+//  I have not been getting these binary trees. Need to study it tomorrow in depth by tracing manually.
+
+//  LeetCode - Lowest Memory
+
+var isSymmetric = function(root) {
+    if(root == null) return true
+    return dfs(root.left, root.right);
+  };
+  
+  function dfs(x, y){
+    if( x == null && y == null) return true;
+    if( x== null ||  y == null) return false;
+    if(x.val !== y.val) return false;
+    
+    return dfs(x.left, y.right) && dfs(x.right, y.left)
+  }
+
+//  LeetCode - Fastest Runtime
+
+var isSymmetric = function(root) {
+    
+    function chk(left, right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        
+        let eq1 = chk(left.left, right.right),
+            eq2 = chk(left.right, right.left);
+        
+        return left.val === right.val && eq1 && eq2;
+    }
+    
+    return chk(root, root);
+    
+};
+
+
