6f28d48fef967a7d93803cdf7b58381d2a7318fa
Alvin Su <su.alvin@proton.me>
2022-11-29 21:56:43 -0800

    11/29 - Update 2

--- _To Remember/js/11 07 - Easy - Valid Parentheses.js
+++ _To Remember/js/11 07 - Easy - Valid Parentheses.js
@@ -12,6 +12,12 @@ Every close bracket has a corresponding open bracket of the same type.
 
 */
 
+/*  Practiced on 11/29 
+    Re-Did Using Regex
+
+    Re-did Leetcode - Lowest Runtime
+*/
+
 //  My Solution 
 
 /*
@@ -20,30 +26,52 @@ Every close bracket has a corresponding open bracket of the same type.
 */
 
 let isValid = function(s) {
-    let regex = /(\(\)|\[\]|\{\})/g;
+  let regex = /(\(\)|\[\]|\{\})/g;
 
-    while ( regex.test(s) ){
-        s = s.replace(regex,'');
-    }
-    
-    return !s;
+  while ( regex.test(s) ){
+      s = s.replace(regex,'');
+  }
+  
+  return !s;
 };
 
-//  Leetcode - Lowest Runtime + Lowest Memory Usage
+//  LeetCode - Lowest Runtime
 
 let isValid = function(s) {
-  const left = [];
-  const legend = {
-    '(': ')',
-    '{': '}',
-    '[': ']'
-  };
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-      left.push(s[i]);    
-    } else if (legend[left.pop()] !== s[i]) {
-      return false;
-    }
+const left = [];
+const legend = {
+  '(': ')',
+  '{': '}',
+  '[': ']'
+};
+for (let i = 0; i < s.length; i++) {
+  if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+    left.push(s[i]);    
+  } else if (legend[left.pop()] !== s[i]) {
+    return false;
   }
-  return left.length ?  0 : 1;
+}
+return left.length ?  0 : 1;
+};
+
+//  LeetCode - Lowest Memory Usage
+
+var isValid = function(s) {
+  let stack_par = [];
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+          stack_par.push(s[i]);
+      } else {
+          if (s[i] === "}" || s[i] === ")" || s[i] === "]") {
+              if (stack_par.length == 0) {
+                  return false;
+              }
+              let current = stack_par.pop();
+              if ((s[i] === "}" && current != "{" )|| (s[i] === "]" && current != "[") || (s[i] === ")" && current != "(")) {
+                  return false;
+              }
+          }
+      }
+  }
+  return stack_par.length ? 0 : 1;
 };
