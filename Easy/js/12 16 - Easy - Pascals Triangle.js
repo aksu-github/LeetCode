af26744c15c575a79a1827a3f4ebc3d5108e78d3
Alvin Su <su.alvin@proton.me>
2022-12-16 22:21:40 -0800

    12/16 - Update 3

---
+++ Easy/js/12 16 - Easy - Pascals Triangle.js
@@ -0,0 +1,45 @@
+/*
+Instruction:
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+*/
+
+//  My Solution 
+
+/*
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let res = [];
+
+    for ( let i = 0; i < numRows; i++){
+        let row = [];
+
+        for ( let j = 0; j <= i; j++){
+            if ( j == 0 || j == i ){
+                row.push(1);
+            } else {
+                row.push( res[i-1][j-1] + res[i-1][j] );
+            }
+        }
+        res.push(row);
+    }
+    return res;
+    
+};
+
+//  Cleaner Version of My Solution - Also Lowest Memory Usage and Runtime
+var generate = function(numRows) {
+    var pascal = [];
+    for (var i = 0; i < numRows; i++) {
+        pascal[i] = [];
+        pascal[i][0] = 1;
+        for (var j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        }
+        pascal[i][i] = 1;
+    }
+    return pascal;
+};
