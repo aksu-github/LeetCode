30d81bc5aca85735a5190723c729e3f7c27a7604
Alvin Su <su.alvin@proton.me>
2023-02-08 22:06:00 -0800

    2023_02_08 - Update 2

---
+++ _To Remember/js/2023/02 08 - Easy - Add Digits.js
@@ -0,0 +1,29 @@
+/*
+Instruction:
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+*/
+
+/*  
+*/
+
+
+//  My Solution
+
+/*
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    return num <= 9 ? num : addDigits(num.toString().split('').map(Number).reduce( (a,b) => a + b, 0));
+};
+
+//  LeetCode - Fastest Runtime + Lowest Memory Usage
+
+let addDigits = function(num) {
+    if (num <= 9) {
+        return num;
+    }
+    return num % 9 === 0 ? 9 : num % 9;
+};
