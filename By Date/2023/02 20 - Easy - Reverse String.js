cb91256e850300af55aa1ddc0b45288063e67869
Alvin Su <su.alvin@proton.me>
2023-02-21 19:01:30 -0800

    2023_02_21 - Update 2

--- By Date/2023/02 20 - Easy - Reverse String.js
+++ By Date/2023/02 20 - Easy - Reverse String.js
@@ -7,7 +7,8 @@ You must do this by modifying the input array in-place with O(1) extra memory.
 
 */ 
 
-/*  
+/*  Practice on 2/21
+    Lowest Memory Usage
 */
 
 //  My Solution - Fastest Runtime
@@ -24,9 +25,18 @@ var reverseString = function(s) {
     }
 };
 
+//  My Solution Using ES6 Notation
+
+var reverseString = function(s) {
+    for ( let i = 0; i < s.length - i; i++){
+        [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+    }
+};
+
 //  LeetCode - Lowest Memory Usage
 
 var reverseString = function(s) {
+    //const mid = s.length / 2 | 0;
     const mid = Math.floor(s.length  / 2)
     for (let l=0, r=s.length - 1; l < mid; l++, r--) { 
         [s[l], s[r]] = [s[r],s[l]]
