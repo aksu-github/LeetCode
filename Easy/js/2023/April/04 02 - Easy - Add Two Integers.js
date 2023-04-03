413042bef1526f3d4aaf17d79f5e74a12455384f
Alvin Su <su.alvin@proton.me>
2023-04-02 22:38:29 -0700

    2023_04_02 - Update 2

---
+++ Easy/js/2023/April/04 02 - Easy - Add Two Integers.js
@@ -0,0 +1,18 @@
+/*
+Instruction:
+
+*/ 
+
+/*  
+*/
+
+//  My Solution
+
+/*
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
