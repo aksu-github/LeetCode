a5c5e26a805c1b03a01a7f45de157a7a983d4662
Alvin Su <su.alvin@proton.me>
2023-05-19 22:01:09 -0700

    2023_05_19 - Update 1

---
+++ Easy/js/2023/May/05 19 - Easy - To Lower Case.js
@@ -0,0 +1,17 @@
+/*
+Instruction:
+
+*/ 
+
+/*  
+*/
+
+//  My Solution - Fastest Runtime + Lowest Memory Usage
+
+/*
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+  return s.toLowerCase();
+};
